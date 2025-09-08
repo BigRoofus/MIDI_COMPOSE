5f5a6180d61087f063b6301ef151aa680214da95
Ryan Maelhorn <ryanmaelhorn@gmail.com>
2025-09-07 23:00:38 -0500

    hype

--- core/vst.py
+++ core/vst.py
@@ -1,3 +1,3 @@
-# This feature manages all aspects of VST integration and audio settings for the app.
-# This includes VST loading/instantiation, the adjustment of VST parameters, 
-# audio interface selection, and the managing any audio-related issues.
+# This feature manages all aspects of VST integration.
+# This includes VST loading/instantiation, the adjustment 
+# of VST parameters, and the unloading/removing of VSTs.
