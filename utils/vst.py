9a350bf8370ab483fe8c05fcc96c84c6fff6c40c
Ryan Maelhorn <ryanmaelhorn@gmail.com>
2025-09-07 22:48:45 -0500

    vet

--- utils/vst.py
+++ utils/vst.py
@@ -1,3 +1,3 @@
 # This feature manages all aspects of VST integration and audio settings for the app.
-# This includes instantiation, audio routing, interface selection, audio-related issues 
-# and the adjustment of VST parameters.
+# This includes VST loading/instantiation, the adjustment of VST parameters, 
+# audio interface selection, and the managing any audio-related issues.
