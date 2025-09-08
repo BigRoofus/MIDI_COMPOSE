dabc2627b805cbd145ee42e3dade7073a533b45e
Ryan Maelhorn <ryanmaelhorn@gmail.com>
2025-09-07 22:56:40 -0500

    not havin it

---
+++ core/audio.py
@@ -0,0 +1,3 @@
+# This feature manages all audio properties and settings for the app.
+# This includes master volume settings, audio interface selection, 
+# and the managing any audio-related issues.
