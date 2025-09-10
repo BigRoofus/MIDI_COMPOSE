c1cd9d543a82bca933913655336686f460419cb5
Ryan Maelhorn <ryanmaelhorn@gmail.com>
2025-09-10 17:43:17 -0500

    page

---
+++ ui/velocity_editor.py
@@ -0,0 +1,14 @@
+# Advanced Velocity Editor
+
+# A completely new and revolutionary Velocity Editor gives you more than 
+# just fine-grained control over individual note dynamics. The Advanced 
+# Velocity Editor provides a much more imaginative and comprehensive 
+# approach. With tools like the Dynamics Curve Tool for three-point 
+# cubic Bezier curve editing, the Freehand Draw Tool for drawing any 
+# dynamic curve by hand, the Mathematical Formula Input Tool for entering 
+# precise math, and the Classic Shapes Tool (triangle, oval, polygon, 
+# invert shape...), the Advanced Velocity Editor gives you next level 
+# control over your composition's dynamics. Want a mathematically precise 
+# 20 bar crescendo? You got it. Want a mathematically correct 
+# $y(t) = 20 \cdot \sin(\pi t)$ 
+# sine wave? You got it.
