0d6f4e611da9e2d8df3ea7a7059dcb6183a63811
Ryan Maelhorn <ryanmaelhorn@gmail.com>
2025-09-10 17:35:32 -0500

    do

--- core/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Core MIDI functionality"""
-from .midi_data import MidiDocument, MidiTrack, MidiNote, MidiEvent, EventType
-from .import_export import MidiImporter, MidiExporter
-
-__all__ = [
-    'MidiDocument', 'MidiTrack', 'MidiNote', 'MidiEvent', 'EventType',
-    'MidiImporter', 'MidiExporter'
-]
