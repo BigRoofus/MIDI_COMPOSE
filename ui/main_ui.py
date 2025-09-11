649d77157a79fff9daaad2922f7e6ed4fb1b64ec
Ryan Maelhorn <ryanmaelhorn@gmail.com>
2025-09-11 18:57:43 -0500

    queen

--- ui/main_ui.py
+++ ui/main_ui.py
@@ -29,3 +29,15 @@ class MainWindow(QMainWindow):
         # Placeholder central widget
         central_widget = QWidget()
         self.setCentralWidget(central_widget)
+
+
+"""
+TRANSPORT CODE
+
+This code is used to create the visual design and logic
+for the Transport control at the top of the application.
+It includes buttons for play/pause, skip to beginning, and 
+skip to end, as well as a BPM display.
+The user may manually change the bpm here.
+"""
+
--- ui/transport.py
+++
@@ -1,7 +0,0 @@
-"""
-This code is used to create the visual design and logic
-for the Transport control at the top of the application.
-It includes buttons for play/pause, skip to beginning, and 
-skip to end, as well as a BPM display.
-The user may manually change the bpm here.
-"""
