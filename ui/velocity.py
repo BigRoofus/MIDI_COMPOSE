fb8b34dd89ac2175df3898afb576b37f280b67e4
Ryan Maelhorn <ryanmaelhorn@gmail.com>
2025-09-11 00:24:54 -0500

    lordan

--- ui/velocity.py
+++ ui/velocity.py
@@ -1,16 +1,15 @@
 """
 Advanced Velocity Editor
 
-A completely new and revolutionary Velocity Editor gives you more than 
-just fine-grained control over individual note dynamics. The Advanced 
-Velocity Editor provides a much more imaginative and comprehensive 
-approach. With tools like the Dynamics Curve Tool for three-point 
-cubic Bezier curve editing, the Freehand Draw Tool for drawing any 
-dynamic curve by hand, the Mathematical Formula Input Tool for entering 
-precise math, and the Classic Shapes Tool (triangle, oval, polygon, 
-invert shape...), the Advanced Velocity Editor gives you next level 
-control over your composition's dynamics. Want a mathematically precise 
-20 bar crescendo? You got it. Want a mathematically correct 
-$y(t) = 20 \cdot \sin(\pi t)$ 
-sine wave? You got it.
+The Advanced Velocity Editor gives the user unprecedented command over a 
+composition's dynamics. More than just offering fine-grained control 
+over individual note dynamics, tools like the Dynamics Curve Tool 
+(for three-point cubic Bezier curves), the Freehand Draw Tool (for 
+freely drawing dynamic curves), the Mathematical Formula Input Tool 
+(for equation-based shaping), and the Classic Shapes Tool (triangle, 
+oval, polygon, invert shape, and more) give the user complete command 
+over any selections's dynamic landscape. If the user wants a 
+mathematically precise 20-bar crescendo, the tools provide it. 
+If the user wants a mathematically correct y ( t ) = 20 ⋅ sin ⁡ ( π t ) 
+sine wave, the tools provide it. 
 """
